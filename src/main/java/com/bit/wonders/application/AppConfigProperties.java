d3f3afb3cfe66553dff241b20ff101e8ac3a154f
dineshchand1984 <dinesh.chand.ait@gmail.com>
2022-01-18 21:43:36 +0530

    Added Integration tests

--- src/main/java/com/bit/wonders/application/AppConfigProperties.java
+++ src/main/java/com/bit/wonders/application/AppConfigProperties.java
@@ -2,11 +2,9 @@ package com.bit.wonders.application;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Data
 @Getter
 @Setter
 @ConfigurationProperties(value="com.bit.wonders.app")
--- src/test/java/com/bit/wonders/application/AppTests.java
+++ src/test/java/com/bit/wonders/application/AppTests.java
@@ -7,17 +7,27 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.bit.wonders.web.HelloController;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("dev")
 public class AppTests {
 
+	@LocalServerPort
+	private int port;
+	
 	@Autowired
 	private HelloController homeController;
 
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+	
 	@Test
 	@DisplayName(value = "Loads application context and verifies if OK.")
 	void contextLoads() {
@@ -25,9 +35,18 @@ public class AppTests {
 	}
 
 	@Test
-	@DisplayName(value = "Test controller function.")
+	@DisplayName(value = "Test controller")
 	void testController() {
 		assertEquals(homeController.projectVersion(), "1.0", "Seems fine");
-		assertEquals(homeController.helloWorld(), "This is dev", "Seems fine");
+		assertEquals(homeController.helloWorld(), "This is dev");
+	}
+	
+	@Test
+	@DisplayName(value = "Test controller using TestRestTemplate")
+	void testTemplate() {
+		assertNotNull(testRestTemplate);
+		ResponseEntity<String> response = testRestTemplate.getForEntity("/hello", String.class);
+		String repsonseText = response.getBody();
+		assertEquals(repsonseText,"This is dev");
 	}
 }
